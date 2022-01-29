3deab29ae373c740c00bc8b5859a83d0000b6041
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-29 16:25:12 +0530

    🧮

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -43,7 +43,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*🏮「 ZeroTwo Broadcast 」🏮*\n\n*🧧 Message:* ${term}\n*☘️ Regards:* *${M.sender.username}*`;
+			const text = `*🏮「 Tengen Broadcast 」🏮*\n\n*🧧 Message:* ${term}\n\n*☘️ Regards:* *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
 				mimetype: Mimetype.gif,
 				caption: `${text}`,
