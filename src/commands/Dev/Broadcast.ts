d8f696b27c91b58f778a24afaf4ad695608aa190
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-29 16:20:32 +0530

    🧮

---
+++ src/commands/Dev/Broadcast.ts
@@ -0,0 +1,57 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "broadcast",
+			description:
+				"Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
+			aliases: ["bcast", "announcement", "bc"],
+			category: "dev",
+			dm: true,
+			usage: `${client.config.prefix}bc`,
+			modsOnly: true,
+			baseXp: 0,
+		});
+	}
+
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		if (!joined)
+			return void (await M.reply(`Please provide the Broadcast Message.`));
+		const term = joined.trim();
+		const gifs = [
+			"https://c.tenor.com/7Ydf6mqbZtYAAAPo/zero-two-anime.mp4",
+			"https://c.tenor.com/Giv4A22u3KUAAAPo/zero-two-smile.mp4",
+			"https://c.tenor.com/KslZTP0KezsAAAPo/zero-two-beach.mp4",
+			"https://c.tenor.com/dE_Ph_z5kz0AAAPo/anime-pink-hair.mp4",
+		];
+		const selected = gifs[Math.floor(Math.random() * gifs.length)];
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const chats: any = this.client.chats
+			.all()
+			.filter((v) => !v.read_only && !v.archive)
+			.map((v) => v.jid)
+			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.filter((v) => v);
+		for (let i = 0; i < chats.length; i++) {
+			const text = `*🏮「 ZeroTwo Broadcast 」🏮*\n\n*🧧 Message:* ${term}\n*☘️ Regards:* *${M.sender.username}*`;
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
+				mimetype: Mimetype.gif,
+				caption: `${text}`,
+				contextInfo: {
+					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
+				},
+			});
+		}
+		await M.reply(` Broadcast Message sent to *${chats.length} groups*.`);
+	};
+}
