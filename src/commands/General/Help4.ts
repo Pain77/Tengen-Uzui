1893745f083502c55f7d2ebfb4bb7bbdce2139f0
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-29 17:45:06 +0530

    Update Help4.ts

--- src/commands/General/Help4.ts
+++ src/commands/General/Help4.ts
@@ -19,11 +19,11 @@ export default class Command extends BaseCommand {
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const nino = 
+		const tengen = 
 			"https://c.tenor.com/pxwtFaHFbzoAAAPo/五等分の花嫁-gotoubun-no-hanayome.mp4";
 		return void this.client.sendMessage(
 			M.from,
-			{ url: nino },
+			{ url: tengen },
 			MessageType.video,
 			{
 				quoted: M.WAMessage,
