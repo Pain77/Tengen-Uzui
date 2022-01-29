f21d1249858dd7e68d8382ede6b1c3711787b87a
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-29 17:30:39 +0530

    Update and rename Zerotwo.ts to Tengen.ts

--- src/commands/Fun/Tengen.ts
+++ src/commands/Fun/Tengen.ts
@@ -7,24 +7,24 @@ import axios from 'axios'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'zerotwo',
-            description: 'Chat with Zerotwo.',
-            aliases: ['02'],
+            command: 'tengen',
+            description: 'Chat with Tengen.',
+            aliases: ['uzui'],
             category: 'fun',
             usage: `${client.config.prefix}zerotwo (text)`
         })
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('_*Darling 🌸*_')
-        const zerotwo = joined.trim()
-        await axios.get(`https://api.simsimi.net/v2/?text=${zerotwo}&lc=en`)
+        if (!joined) return void M.reply('_*Flamboyant 🌸*_')
+        const tengen = joined.trim()
+        await axios.get(`https://api.simsimi.net/v2/?text=${tengen}&lc=en`)
         .then((response) => {
                 // console.log(response);
-                const text = `_*🎈Zero Two:*_  ${response.data.success}`
+                const text = `_*🎈Tengen Uzui:*_  ${response.data.success}`
                 M.reply(text);
             }).catch(err => {
-                M.reply(`_*Darling 🌸*_`)
+                M.reply(`_*Flamboyant 🌸*_`)
             }
             )
     };
