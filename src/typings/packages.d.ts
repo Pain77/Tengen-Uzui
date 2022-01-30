50982f3722269c87591deeac3222376daf78d79c
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-30 14:11:18 +0530

    ⌨️

---
+++ src/typings/packages.d.ts
@@ -0,0 +1,26 @@
+/** @format */
+
+declare module "akaneko";
+declare module "lolis.life";
+declare module "mathjs";
+declare module "node-fetch";
+declare module "genshin-db";
+declare module "shentai";
+declare module "mywaifulist-scraper";
+declare module "mangadex-full-api";
+declare module "mal-scraper";
+declare module "wikipedia";
+declare module "anime-wallpapers";
+declare module "translate-google";
+declare module "animequotes";
+declare module "mailist";
+declare module "node-periodic-table";
+declare module "oakdex-pokedex";
+declare module "ptable";
+declare module "node-myanimelist";
+declare module "genius-lyrics";
+declare module "trace.moe.ts";
+declare module "anilist-node";
+declare module "pokedex-promise-v2";
+declare module 'imgbb-uploader'
+declare module 'text-from-image'
