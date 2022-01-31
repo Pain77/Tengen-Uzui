9c673eb4e535c9a996724871c12b5f8cf738651e
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-31 16:48:02 +0530

    Update Help3.ts

--- src/commands/General/Help3.ts
+++ src/commands/General/Help3.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const tengen = 
-			"https://c.tenor.com/2Y-jDlehTzUAAAPo/nino-nakano-nino.mp4";
+			"https://c.tenor.com/CW9mN1LHWOMAAAPo/demon-slayer-kimetsu-no-yaiba.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: tengen },
