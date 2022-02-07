38fd6df30ec682bbc806eb8385f90588b79e2359
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-02-07 13:12:31 +0530

    Update Help7.ts

--- src/commands/General/Help7.ts
+++ src/commands/General/Help7.ts
@@ -12,7 +12,7 @@ export default class Command extends BaseCommand {
 			command: "help7",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}nino`,
+			usage: `${client.config.prefix}tengen`,
 		        dm: true,
                         aliases: ['h7','?7','menu7']
 		});
